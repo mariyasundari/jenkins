c04a28f75d5addc703fef54b78d952f30642843e
mariyakevin2020@gmail.com <mariyakevin2020@gmail.com>
2025-04-10 11:28:43 +0530

    jenkins

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Main {
+@Test
+        public void display()
+{
+    System.out.println(" Career Ladder");
+}
+@Test
+    public void display2()
+{
+    System.out.println("I am Career Ladder");
+}
+}
